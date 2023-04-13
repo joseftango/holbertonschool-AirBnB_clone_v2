e0db06ef66b874e7dd9c20ca76351cad7923f6ea
joseftango <4279@holbertonschool.com>
2023-04-13 12:48:15 +0100

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,8 +2,9 @@
 """module named 0-hello_route"""
 from flask import Flask
 
-
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def display_1():
     """function that display text in page"""
