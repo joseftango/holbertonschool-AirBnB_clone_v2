97ce152e5f1dd49a3be09a7ca8587a78b83a41a3
joseftango <4279@holbertonschool.com>
2023-04-16 19:00:53 +0100

    8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module named 7-states_list"""
+from flask import Flask, render_template
+from models import storage, state, city
+
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def display_template():
+    """displays a template with a fetched
+    data from the storage engine"""
+    state_list = storage.all(state.State).values()
+    city_list = []
+    for st in state_list:
+        city_list.append(st.cities)
+    return render_template("7-states_list.html",
+                           title="States", states=state_list, cities=city_list)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
