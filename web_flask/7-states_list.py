5488879d683f69406c938f6d0cfcf5b595cc9813
joseftango <4279@holbertonschool.com>
2023-04-16 14:11:33 +0100

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,14 +6,13 @@ from models import storage, state
 
 app = Flask(__name__)
 
-
 @app.route("/states_list", strict_slashes=False)
 def display_template():
     """displays a template with a fetched
     data from the storage engine"""
-    state_dict = storage.all(state.State).values()
+    state_list = storage.all(state.State).values()
     return render_template("7-states_list.html",
-                           title="States", states=state_dict)
+                           title="States", states=state_list)
 
 
 if __name__ == '__main__':
