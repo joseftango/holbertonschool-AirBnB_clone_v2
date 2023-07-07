bc536f27e1d0158f3e36de1b30ab3b6adbd56b53
joseftango <4279@holbertonschool.com>
2023-07-07 20:22:48 +0100

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,14 +7,18 @@ from models import storage, state
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def display_template():
-    """displays a template with a fetched
-    data from the storage engine"""
-    state_list = storage.all(state.State).values()
-    return render_template("7-states_list.html",
-                           title="States", states=state_list)
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """Displays html page"""
+    states = storage.all(state.State).values()
+    return render_template('7-states_list.html', my_dict=states)
+
+
+@app.teardown_appcontext
+def close_sqlalchemy_sess(exception):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
