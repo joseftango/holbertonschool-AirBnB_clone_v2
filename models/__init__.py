7d19b943465c2822bc079c58879e50feb856207f
joseftango <4279@holbertonschool.com>
2025-03-01 15:13:53 +0100

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object
 of class FileStorage and DBStorage"""
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from os import getenv
 
 
