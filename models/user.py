d2d9dd31fb0bf32d277618e2443414ff93110377
joseftango <4279@holbertonschool.com>
2025-03-02 03:23:30 +0100

    models

--- models/user.py
+++ models/user.py
@@ -15,6 +15,9 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship('Place',
+                              back_populates="user",
+                              cascade="all, delete")
     else:
         email = ''
         password = ''
