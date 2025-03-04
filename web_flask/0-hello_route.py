c588f1be87fe8a6e33a1356385497b183c9db499
joseftango <4279@holbertonschool.com>
2025-03-04 19:22:55 +0100

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""0-hello_route Module"""
+'''0-hello_route Module'''
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def index():
-    """Displays 'Hello HBNB!' upon visiting root page"""
+    '''displays the returned text'''
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0')
