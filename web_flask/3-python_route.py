e590b08f469c4a3398bc10fafe96b577097df5f9
joseftango <4279@holbertonschool.com>
2025-03-05 15:48:01 +0100

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''0-hello_route Module'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def greed():
+    '''displays "Hello HBNB!"'''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    '''displays "HBNB"'''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_c(text):
+    '''display the character "C" followed
+    by space and argument'''
+    spaced_text = text.replace('_', ' ')
+    return f"C {spaced_text}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def display_python(text='is cool'):
+    '''display the character "python" followed
+    by space and argument'''
+    spaced_text = text.replace('_', ' ')
+    return f"python {spaced_text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
