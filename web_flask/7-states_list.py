27df10faa7f1312effc23943da2841edef1901f0
joseftango <4279@holbertonschool.com>
2025-03-06 02:10:07 +0100

    templates

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''0-hello_route Module'''
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(exception=None):
+    '''close session when app context is torn down'''
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    '''display the every state and
+    the linked cities from DB'''
+    states = storage.all(State)
+    states = states.values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
