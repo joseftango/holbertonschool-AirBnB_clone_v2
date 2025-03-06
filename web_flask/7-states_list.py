ada353fcc1532dc4c9b0f00e7fa58f87f60a2fc5
joseftango <4279@holbertonschool.com>
2025-03-06 02:31:19 +0100

    templates

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,20 +8,19 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def close_session(exception=None):
-    '''close session when app context is torn down'''
-    storage.close()
-
-
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     '''display the every state and
     the linked cities from DB'''
-    states = storage.all(State)
-    states = states.values()
+    states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def close_session(exception=None):
+    '''close session when app context is torn down'''
+    storage.close()
 
 
 if __name__ == '__main__':
